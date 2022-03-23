43fab206a9266271c4897bd3bcb8ea46d27cf97a
16Hurley_J <jpjhurley@gmail.com>
2022-03-23 13:56:58 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String test = "true";
+
+        switch(test){
+            case "true" :
+                //goes to test
+            case "false":
+                //goes to table
+        }
+
+    }
+}
